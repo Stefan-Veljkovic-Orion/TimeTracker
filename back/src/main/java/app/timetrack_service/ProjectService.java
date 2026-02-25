8de79172a2d420c9a6368c7332abe0675d18012d
prosenica-orion <andjela.prosenica@orioninc.com>
2026-02-25 14:30:17 +0100

    Created feature deleteProject

--- back/src/main/java/app/timetrack_service/ProjectService.java
+++ back/src/main/java/app/timetrack_service/ProjectService.java
@@ -2,7 +2,9 @@ package app.timetrack_service;
 
 import app.timetrack_repository.IProjectRepository;
 import app.timetracker_entity_implemantion.Project;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -37,5 +39,20 @@ public class ProjectService {
         return projectRepository.save(project);
     }
 
+    public void deleteProject(int id) {
+
+        Project project = projectRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Project not found"));
+
+        if (project.getActivities() != null && !project.getActivities().isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Can't delete project because it has activities linked to it"
+            );
+        }
+
+        projectRepository.delete(project);
+    }
 
 }
--- back/src/main/java/app/timetracker_entity_implemantion/Project.java
+++ back/src/main/java/app/timetracker_entity_implemantion/Project.java
@@ -13,7 +13,7 @@ public class Project {
     private String projectName;
     private String description;
     private String managerEmail;
-    @OneToMany(mappedBy = "project",cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "project",cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<Activity> activities;
 
     public Project(int id, String projectName, String description, String managerEmail, List<Activity> activities) {
