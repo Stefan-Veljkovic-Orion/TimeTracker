16b32feb87aeee805e36e777f063fe4e057e8a62
prosenica-orion <andjela.prosenica@orioninc.com>
2026-02-25 09:28:18 +0100

    Fixed git ignore

--- back/src/main/java/app/timetrack_repository/IProjectRepository.java
+++ back/src/main/java/app/timetrack_repository/IProjectRepository.java
@@ -1,11 +1,9 @@
 package app.timetrack_repository;
 
 import app.timetracker_entity_implemantion.Project;
-import org.springframework.data.jpa.repository.JpaContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface IProjectRepository extends JpaRepository<Project, Integer> {
