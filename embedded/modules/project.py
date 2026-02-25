2952a69f941874a1c92857adde74f9066620b965
andjelija-djuric <andjelija.djuric@orioninc.com>
2026-02-25 12:57:04 +0100

    Added comments in project.py module

--- embedded/modules/project.py
+++ embedded/modules/project.py
@@ -1,6 +1,9 @@
 from utils.storage import load_data, save_locally, PROJECTS_FILE
 from utils.validators import validate_project
 
+
+
+# function for creating a project
 def create_project():
     name = input("Enter project name: ")
     description = input("Enter project description (max 150 chars): ")
@@ -11,5 +14,5 @@ def create_project():
         
     projects = load_data(PROJECTS_FILE)
     projects.append({"name": name, "description": description, "manager_email": manager_email})
-    save_locally(PROJECTS_FILE, projects)
+    save_locally(PROJECTS_FILE, projects)  
     print(f"Project {name} created with manager {manager_email}!")
